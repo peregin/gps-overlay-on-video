23a1c9e2e05c617abc5aed091191f55447a65251
peregin <info@peregin.com>
2014-04-08 23:04:46 +0200

    show a video frame

--- src/main/scala/peregin/tov/gui/VideoPanel.scala
+++ src/main/scala/peregin/tov/gui/VideoPanel.scala
@@ -3,6 +3,12 @@ package peregin.tov.gui
 import java.io.File
 import javax.swing.filechooser.FileNameExtensionFilter
 import peregin.tov.model.Setup
+import com.xuggle.mediatool.{MediaToolAdapter, ToolFactory}
+import java.awt.image.BufferedImage
+import com.xuggle.mediatool.event.IVideoPictureEvent
+import javax.swing.JPanel
+import java.awt.{Color, Graphics, Image}
+import scala.swing.Component
 
 
 class VideoPanel(openVideoData: File => Unit) extends MigPanel("ins 2", "", "[fill]") {
@@ -10,7 +16,46 @@ class VideoPanel(openVideoData: File => Unit) extends MigPanel("ins 2", "", "[fi
   val chooser = new FileChooserPanel("Load video file:", openVideoData, new FileNameExtensionFilter("Video files (mp4)", "mp4"))
   add(chooser, "pushx, growx, wrap")
 
+  class ImagePanel extends JPanel {
+    var image: Image = null
+
+    def show(im: Image) {
+      image = im
+      repaint()
+    }
+
+    override def paint(g: Graphics) = {
+      g.setColor(Color.yellow)
+      val width = getWidth
+      val height = getHeight
+      g.fillRect(0, 0, width, height)
+
+      if (image != null) {
+        val w = image.getWidth(null)
+        val h = image.getHeight(null)
+        val x = (width - w) / 2
+        val y = (height - h) / 2
+        g.drawImage(image, x, y, w, h, null)
+      }
+    }
+  }
+  val imagePanel = new ImagePanel
+  add(Component.wrap(imagePanel), "grow, pushy, wrap")
+
   def refresh(setup: Setup) {
     chooser.fileInput.text = setup.videoPath.getOrElse("")
+    setup.videoPath.foreach{path =>
+      val mediaReader = ToolFactory.makeReader(path)
+      mediaReader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR)
+      mediaReader.addListener(new MediaToolAdapter {
+        override def onVideoPicture(event: IVideoPictureEvent) = {
+
+          imagePanel.show(event.getImage)
+
+          super.onVideoPicture(event)
+        }
+      })
+      (1 until 100)foreach(_ => mediaReader.readPacket())
+    }
   }
 }
