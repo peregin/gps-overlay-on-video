17ce770ba2deecb24dfc27599503359ef978841d
peregin <info@peregin.com>
2014-04-13 22:16:54 +0200

    manual test for gauges

--- src/main/scala/peregin/gpv/gui/GaugeComponent.scala
+++ src/main/scala/peregin/gpv/gui/GaugeComponent.scala
@@ -1,8 +1,10 @@
 package peregin.gpv.gui
 
 import scala.swing.Component
+import java.awt.Dimension
 
 
 abstract class GaugeComponent extends Component {
 
+  preferredSize = new Dimension(75, 75)
 }
---
+++ src/test/scala/peregin/gpv/manual/GaugeManualTest.scala
@@ -0,0 +1,38 @@
+package peregin.gpv.manual
+
+import scala.swing._
+import java.awt.{Color, Dimension}
+import peregin.gpv.gui.{Goodies, MigPanel}
+import peregin.gpv.gui.gauge.SpeedGauge
+import scala.swing.event.ButtonClicked
+import peregin.gpv.util.Logging
+
+
+object GaugeManualTest extends SimpleSwingApplication with Logging {
+
+  val frame = new MainFrame {
+    title = "Gauge Test Container"
+    contents = new MigPanel("ins 5, fill", "[fill]", "[fill]") {
+      background = Color.lightGray
+
+      add(new SpeedGauge, "")
+      add(new SpeedGauge, "wrap")
+      add(new SpeedGauge, "span 2, w 150px, h 150px, wrap")
+      val testButton = new Button("Test")
+      add(testButton, "wrap")
+
+      listenTo(testButton)
+      reactions += {
+        case ButtonClicked(`testButton`) => testGauge
+      }
+    }
+  }
+  frame.size = new Dimension(1024, 768)
+  Goodies.center(frame)
+
+  override def top = frame
+
+  def testGauge {
+    log.info("test")
+  }
+}
