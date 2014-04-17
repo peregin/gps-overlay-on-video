9319ec22905daefb38a5d1d11ca8193e05da87a1
peregin <info@peregin.com>
2014-04-17 21:02:15 +0200

    added controls to test the gauge

--- src/test/scala/peregin/gpv/manual/GaugeManualTest.scala
+++ src/test/scala/peregin/gpv/manual/GaugeManualTest.scala
@@ -11,21 +11,28 @@ import javax.swing.{JSlider, JSpinner}
 
 object GaugeManualTest extends SimpleSwingApplication with Logging {
 
+  val minSpinner = new JSpinner
+  minSpinner.setValue(0)
+  val maxSpinner = new JSpinner
+  maxSpinner.setValue(75)
+  val slider = new JSlider(0, 75, 32)
+  val status = new Label(s"Current Value ${slider.getValue}")
+
   val frame = new MainFrame {
     title = s"Gauge Test Container - built ${BuildInfo.buildTime}"
     contents = new MigPanel("ins 5, fill", "[fill]", "[fill]") {
       background = Color.lightGray
 
       add(new SpeedGauge, "w 50px, h 50px")
-      add(new Label("Current Value"), "wrap")
+      add(status, "wrap")
       add(new SpeedGauge, "w 100px, h 100px, wrap")
       add(new SpeedGauge, "w 200px, h 200px")
       add(new SpeedGauge, "w 300px, h 300px, wrap")
-      val controls = new MigPanel("", "", "") {
+      val controls = new MigPanel("", "[fill]", "[fill]") {
         add(new Label("Min"), "alignx left")
-        add(new JSpinner, "alignx left")
-        add(new JSlider, "pushx, growx")
-        add(new JSpinner, "alignx left")
+        add(minSpinner, "alignx left, wmin 50")
+        add(slider, "pushx, growx")
+        add(maxSpinner, "alignx left, wmin 50")
         add(new Label("Max"), "alignx right")
       }
       add(controls, "span 2, wrap")
