2c2a60e992af074372e5e10f36c648031d993b53
peregin <info@peregin.com>
2014-04-17 20:08:16 +0200

    gauge tester layout

--- src/test/scala/peregin/gpv/manual/GaugeManualTest.scala
+++ src/test/scala/peregin/gpv/manual/GaugeManualTest.scala
@@ -5,8 +5,8 @@ import scala.swing._
 import java.awt.Color
 import peregin.gpv.gui.{Goodies, MigPanel}
 import peregin.gpv.gui.gauge.SpeedGauge
-import scala.swing.event.ButtonClicked
 import peregin.gpv.util.Logging
+import javax.swing.{JSlider, JSpinner}
 
 
 object GaugeManualTest extends SimpleSwingApplication with Logging {
@@ -16,24 +16,22 @@ object GaugeManualTest extends SimpleSwingApplication with Logging {
     contents = new MigPanel("ins 5, fill", "[fill]", "[fill]") {
       background = Color.lightGray
 
-      add(new SpeedGauge, "w 50px, h 50px, wrap")
+      add(new SpeedGauge, "w 50px, h 50px")
+      add(new Label("Current Value"), "wrap")
       add(new SpeedGauge, "w 100px, h 100px, wrap")
       add(new SpeedGauge, "w 200px, h 200px")
       add(new SpeedGauge, "w 300px, h 300px, wrap")
-      val testButton = new Button("Test")
-      add(testButton, "wrap")
-
-      listenTo(testButton)
-      reactions += {
-        case ButtonClicked(`testButton`) => testGauge
+      val controls = new MigPanel("", "", "") {
+        add(new Label("Min"), "alignx left")
+        add(new JSpinner, "alignx left")
+        add(new JSlider, "pushx, growx")
+        add(new JSpinner, "alignx left")
+        add(new Label("Max"), "alignx right")
       }
+      add(controls, "span 2, wrap")
     }
   }
   Goodies.center(frame)
 
   override def top = frame
-
-  def testGauge {
-    log.info("test")
-  }
 }
