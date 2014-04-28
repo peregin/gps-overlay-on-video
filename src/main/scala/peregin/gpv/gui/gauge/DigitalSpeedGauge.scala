e80fdd8698d8dd56c4e58fae07f8bcc3cc852880
peregin <info@peregin.com>
2014-04-28 20:20:52 +0200

    add km/h label to the speed gauge

--- src/main/scala/peregin/gpv/gui/gauge/DigitalSpeedGauge.scala
+++ src/main/scala/peregin/gpv/gui/gauge/DigitalSpeedGauge.scala
@@ -24,9 +24,9 @@ class DigitalSpeedGauge extends GaugeComponent with DigitalFont {
     val tb = g.getFontMetrics.getStringBounds(text, g)
     textWidthShadow(g, text, (w - tb.getWidth) / 2, cy + box / 2 - tb.getHeight * 1.2)
     // draw unit
-    //g.setFont(digitalFont.deriveFont(Font.BOLD, fs.toFloat))
-//    val utext = "km/h"
-//    val utb = g.getFontMetrics.getStringBounds(utext, g)
-//    textWidthShadow(g, utext, (w - utb.getWidth) / 2, cy + box / 2 - utb.getHeight * 3)
+    g.setFont(digitalFont.deriveFont(Font.BOLD, fs / 4))
+    val utext = "km/h"
+    val utb = g.getFontMetrics.getStringBounds(utext, g)
+    textWidthShadow(g, utext, (w - utb.getWidth) / 2, cy + box / 2 + utb.getHeight * 2 - tb.getHeight * 1.2)
   }
 }
