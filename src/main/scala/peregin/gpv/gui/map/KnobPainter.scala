48154b94f939b57b58ba9746214e8e408eac6616
peregin <info@peregin.com>
2014-05-14 22:01:01 +0200

    drop shadow and edge for the knob painter

--- src/main/scala/peregin/gpv/gui/map/KnobPainter.scala
+++ src/main/scala/peregin/gpv/gui/map/KnobPainter.scala
@@ -1,15 +1,22 @@
 package peregin.gpv.gui.map
 
-import java.awt.{Color, Graphics2D}
+import java.awt.{BasicStroke, Color, Graphics2D}
 import java.awt.geom.Point2D
 
 
 trait KnobPainter {
 
+  lazy val edge = new BasicStroke(2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 10.0f, null, 0.0f)
+
   def paintKnob(g: Graphics2D, pt: Point2D, color: Color): Unit = paintKnob(g, pt.getX.toInt, pt.getY.toInt, color)
 
   def paintKnob(g: Graphics2D, x: Int, y: Int, color: Color) {
+    g.setColor(Color.gray)
+    g.fillOval(x - 3, y - 3, 10, 10)
     g.setColor(color)
     g.fillOval(x - 5, y - 5, 10, 10)
+    g.setColor(Color.black)
+    g.setStroke(edge)
+    g.drawOval(x - 5, y - 5, 10, 10)
   }
 }
