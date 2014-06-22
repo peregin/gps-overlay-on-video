d84708cd1e3edd52ecc05af93f53c9198459ddce
peregin <info@peregin.com>
2014-06-22 22:35:21 +0200

    prepare instant seek with the slider

--- src/main/scala/peregin/gpv/gui/video/ExperimentalVideoPlayer.scala
+++ src/main/scala/peregin/gpv/gui/video/ExperimentalVideoPlayer.scala
@@ -15,14 +15,14 @@ class ExperimentalVideoPlayer(url: String, telemetry: Telemetry,
 
   // Let's make sure that we can actually convert video pixel formats.
   if (!IVideoResampler.isSupported(IVideoResampler.Feature.FEATURE_COLORSPACECONVERSION))
-    throw new RuntimeException("you must install the GPL version of Xuggler (with IVideoResampler support) ")
+    throw new RuntimeException("you must install the GPL version of Xuggler (with IVideoResampler support)")
 
   // Create a Xuggler container object
   val container = IContainer.make()
 
   // Open up the container
   if (container.open(url, IContainer.Type.READ, null) < 0)
-    throw new IllegalArgumentException("could not open file: " + url)
+    throw new IllegalArgumentException(s"could not open file: $url")
 
   val durationInMillis = container.getDuration / 1000
   info(s"duration: ${DurationPrinter.print(durationInMillis)}")
@@ -38,7 +38,7 @@ class ExperimentalVideoPlayer(url: String, telemetry: Telemetry,
   for (i <- 0 until numStreams) {
     // Find the stream object
     val stream = container.getStream(i)
-    // Get the pre-configured decoder that can decode this stream;
+    // Get the pre-configured decoder that can decode this stream
     val coder = stream.getStreamCoder()
 
     if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO) {
@@ -48,15 +48,14 @@ class ExperimentalVideoPlayer(url: String, telemetry: Telemetry,
       info(f"frame rate: $frameRate%5.2f")
     }
   }
-  if (videoStreamId == -1)
-    throw new RuntimeException("could not find video stream in container: " + url)
+  if (videoStreamId == -1) throw new RuntimeException(s"could not find video stream in container: $url")
 
   /*
    * Now we have found the video stream in this file.  Let's open up our decoder so it can
    * do work.
    */
   if (videoCoder.open() < 0)
-    throw new RuntimeException("could not open video decoder for container: " + url)
+    throw new RuntimeException(s"could not open video decoder for container: $url")
 
   var resampler: IVideoResampler = null
   if (videoCoder.getPixelType() != IPixelFormat.Type.BGR24) {
@@ -65,10 +64,10 @@ class ExperimentalVideoPlayer(url: String, telemetry: Telemetry,
     resampler = IVideoResampler.make(videoCoder.getWidth(),
       videoCoder.getHeight(), IPixelFormat.Type.BGR24,
       videoCoder.getWidth(), videoCoder.getHeight(), videoCoder.getPixelType())
-    if (resampler == null)
-      throw new RuntimeException("could not create color space " +
-        "resampler for: " + url)
+    if (resampler == null) throw new RuntimeException(s"could not create color space resampler for: $url")
   }
+
+  val packet = IPacket.make()
 
   import scala.concurrent._
   import ExecutionContext.Implicits.global
@@ -80,112 +79,106 @@ class ExperimentalVideoPlayer(url: String, telemetry: Telemetry,
     /*
      * Now, we start walking through the container looking at each packet.
      */
-    val packet = IPacket.make()
     var firstTimestampInStream = Global.NO_PTS
     var systemClockStartTime = 0L
     while (container.readNextPacket(packet) >= 0) {
-      /*
-     * Now we have a packet, let's see if it belongs to our video stream
-     */
-      if (packet.getStreamIndex() == videoStreamId) {
         /*
-       * We allocate a new picture to get the data out of Xuggler
-       */
-        val picture = IVideoPicture.make(videoCoder.getPixelType(),
-          videoCoder.getWidth(), videoCoder.getHeight())
-
-        var offset = 0
-        while (offset < packet.getSize()) {
-          /*
-         * Now, we decode the video, checking for any errors.
-         *
+         * Now we have a packet, let's see if it belongs to our video stream
          */
-          val bytesDecoded = videoCoder.decodeVideo(picture, packet, offset)
-          if (bytesDecoded < 0)
-            throw new RuntimeException("got error decoding video in: " + url);
-          offset += bytesDecoded;
-
+        if (packet.getStreamIndex() == videoStreamId) {
           /*
+           * We allocate a new picture to get the data out of Xuggler
+           */
+          val picture = IVideoPicture.make(videoCoder.getPixelType(), videoCoder.getWidth(), videoCoder.getHeight())
+
+          var offset = 0
+          while (offset < packet.getSize()) {
+            /*
+             * Now, we decode the video, checking for any errors.
+             *
+             */
+            val bytesDecoded = videoCoder.decodeVideo(picture, packet, offset)
+            if (bytesDecoded < 0) throw new RuntimeException(s"got error decoding video in: $url")
+            offset += bytesDecoded
+
+            /*
          * Some decoders will consume data in a packet, but will not be able to construct
          * a full video picture yet.  Therefore you should always check if you
          * got a complete picture from the decoder
          */
-          if (picture.isComplete()) {
-            var newPic: IVideoPicture = picture
-            /*
+            if (picture.isComplete()) {
+              var newPic: IVideoPicture = picture
+              /*
            * If the resampler is not null, that means we didn't get the
            * video in BGR24 format and
            * need to convert it into BGR24 format.
            */
-            if (resampler != null) {
-              // we must resample
-              newPic = IVideoPicture.make(resampler.getOutputPixelFormat(),
-                picture.getWidth(), picture.getHeight());
-              if (resampler.resample(newPic, picture) < 0)
-                throw new RuntimeException("could not resample video from: "
-                  + url);
-            }
-            if (newPic.getPixelType() != IPixelFormat.Type.BGR24)
-              throw new RuntimeException("could not decode video" +
-                " as BGR 24 bit data in: " + url);
-
-            /**
-             * We could just display the images as quickly as we decode them,
-             * but it turns out we can decode a lot faster than you think.
-             *
-             * So instead, the following code does a poor-man's version of
-             * trying to match up the frame-rate requested for each
-             * IVideoPicture with the system clock time on your computer.
-             *
-             * Remember that all Xuggler IAudioSamples and IVideoPicture objects
-             * always give timestamps in Microseconds, relative to the first
-             * decoded item. If instead you used the packet timestamps, they can
-             * be in different units depending on your IContainer, and IStream
-             * and things can get hairy quickly.
-             */
-            if (firstTimestampInStream == Global.NO_PTS) {
-              // This is our first time through
-              firstTimestampInStream = picture.getTimeStamp();
-              // get the starting clock time so we can hold up frames
-              // until the right time.
-              systemClockStartTime = System.currentTimeMillis();
-            } else {
-              val systemClockCurrentTime = System.currentTimeMillis();
-              val millisecondsClockTimeSinceStartofVideo =
-                systemClockCurrentTime - systemClockStartTime;
-              // compute how long for this frame since the first frame in the stream.
-              // remember that IVideoPicture and IAudioSamples timestamps are always in MICROSECONDS,
-              // so we divide by 1000 to get milliseconds.
-              val millisecondsStreamTimeSinceStartOfVideo = (picture.getTimeStamp() - firstTimestampInStream) / 1000
-              val millisecondsTolerance = 50L // and we give ourselfs 50 ms of tolerance
-              val millisecondsToSleep = (millisecondsStreamTimeSinceStartOfVideo -
-                  (millisecondsClockTimeSinceStartofVideo + millisecondsTolerance))
-              if (millisecondsToSleep > 0) {
-                Thread.sleep(millisecondsToSleep)
+              if (resampler != null) {
+                // we must resample
+                newPic = IVideoPicture.make(resampler.getOutputPixelFormat(), picture.getWidth(), picture.getHeight())
+                if (resampler.resample(newPic, picture) < 0)
+                  throw new RuntimeException(s"could not resample video from: $url")
               }
+              if (newPic.getPixelType() != IPixelFormat.Type.BGR24)
+                throw new RuntimeException(s"could not decode video as BGR 24 bit data in: $url")
+
+              /**
+               * We could just display the images as quickly as we decode them,
+               * but it turns out we can decode a lot faster than you think.
+               *
+               * So instead, the following code does a poor-man's version of
+               * trying to match up the frame-rate requested for each
+               * IVideoPicture with the system clock time on your computer.
+               *
+               * Remember that all Xuggler IAudioSamples and IVideoPicture objects
+               * always give timestamps in Microseconds, relative to the first
+               * decoded item. If instead you used the packet timestamps, they can
+               * be in different units depending on your IContainer, and IStream
+               * and things can get hairy quickly.
+               */
+              if (firstTimestampInStream == Global.NO_PTS) {
+                // This is our first time through
+                firstTimestampInStream = picture.getTimeStamp()
+                // get the starting clock time so we can hold up frames
+                // until the right time.
+                systemClockStartTime = System.currentTimeMillis()
+              } else {
+                val systemClockCurrentTime = System.currentTimeMillis()
+                val millisecondsClockTimeSinceStartofVideo =
+                  systemClockCurrentTime - systemClockStartTime
+                // compute how long for this frame since the first frame in the stream.
+                // remember that IVideoPicture and IAudioSamples timestamps are always in MICROSECONDS,
+                // so we divide by 1000 to get milliseconds.
+                val millisecondsStreamTimeSinceStartOfVideo = (picture.getTimeStamp() - firstTimestampInStream) / 1000
+                val millisecondsTolerance = 50L // and we give ourselfs 50 ms of tolerance
+                val millisecondsToSleep = (millisecondsStreamTimeSinceStartOfVideo -
+                    (millisecondsClockTimeSinceStartofVideo + millisecondsTolerance))
+                if (millisecondsToSleep > 0) {
+                  Thread.sleep(millisecondsToSleep)
+                }
+              }
+
+              val tsInMillis = picture.getTimeStamp / 1000
+              val percentage = if (durationInMillis > 0) tsInMillis * 100 / durationInMillis else 0
+              timeUpdater(tsInMillis, percentage.toInt)
+
+              // And finally, convert the BGR24 to an Java buffered image
+              val javaImage = Utils.videoPictureToImage(newPic)
+
+              // and display it on the Java Swing window
+              imageHandler(javaImage)
             }
-
-            val tsInMillis = picture.getTimeStamp / 1000
-            val percentage = if (durationInMillis > 0) tsInMillis * 100 / durationInMillis else 0
-            timeUpdater(tsInMillis, percentage.toInt)
-
-            // And finally, convert the BGR24 to an Java buffered image
-            val javaImage = Utils.videoPictureToImage(newPic)
-
-            // and display it on the Java Swing window
-            imageHandler(javaImage)
           }
-        }
-      }
-      else {
-        /*
+        } else {
+          /*
        * This packet isn't part of our video stream, so we just
        * silently drop it.
        */
-        do {} while (false);
-      }
+          do {} while (false)
+        }
 
-    }
+
+    } // while
 
     close()
   }
@@ -200,6 +193,29 @@ class ExperimentalVideoPlayer(url: String, telemetry: Telemetry,
     val frames = durationInMillis / 1000 * frameRate
     val jumpToFrame = frames * p / 100
     container.seekKeyFrame(videoStreamId, jumpToFrame.toLong, IContainer.SEEK_FLAG_FRAME)
+  }
+
+  private def doSeekVideo(seek: Long, packet: IPacket, picture: IVideoPicture) {
+    val rational = videoCoder.getTimeBase()
+    val seekTime = seek * rational.getDenominator() / rational.getNumerator() / 1000
+    container.seekKeyFrame(videoStreamId, 0, seekTime, seekTime, 0)
+    val pictureTime = seek * 1000
+    while (container.readNextPacket(packet) >= 0) {
+      if (packet.getStreamIndex() == videoStreamId) {
+        var offset = 0
+        while (offset < packet.getSize()) {
+          val bytesDecoded = videoCoder.decodeVideo(picture, packet, offset)
+          if (bytesDecoded < 0) {
+            throw new RuntimeException("got error decoding video in:")
+          }
+          offset += bytesDecoded
+
+          if (picture.isComplete() && picture.getTimeStamp() >= pictureTime) {
+            return
+          }
+        }
+      }
+    }
   }
 
   override def close() {
