076574c67fd512a522e822575e64661ba9a4fc4a
peregin <info@peregin.com>
2014-10-07 20:18:15 +0200

    simple factory methods to create different players

--- src/main/scala/peregin/gpv/gui/video/VideoPlayer.scala
+++ src/main/scala/peregin/gpv/gui/video/VideoPlayer.scala
@@ -1,5 +1,9 @@
 package peregin.gpv.gui.video
 
+import java.awt.Image
+
+import peregin.gpv.model.Telemetry
+
 
 // extract the expected functionality from the player into this interface
 // implementation can be easily replaced (media vs tool based from xuggler)
@@ -8,4 +12,19 @@ trait VideoPlayer {
   def seek(percentage: Double)
 
   def close()
+}
+
+object VideoPlayer {
+
+  type inputType = (String, Telemetry, (Image) => Unit, () => Long, (Long, Int) => Unit)
+
+  //def create(t: inputType) = new ExperimentalVideoPlayer()
+
+  def createSimple(url: String, telemetry: Telemetry, imageHandler: Image => Unit,
+             shiftHandler: => Long, timeUpdater: (Long, Int) => Unit) =
+    new SimpleVideoPlayer(url, telemetry, imageHandler, shiftHandler, timeUpdater)
+
+  def createExperimental(url: String, telemetry: Telemetry, imageHandler: Image => Unit,
+                         shiftHandler: => Long, timeUpdater: (Long, Int) => Unit) =
+    new ExperimentalVideoPlayer(url, telemetry, imageHandler, shiftHandler, timeUpdater)
 }
