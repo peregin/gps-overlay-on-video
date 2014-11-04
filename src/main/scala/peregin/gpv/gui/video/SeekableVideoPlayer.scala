1d588d7c5307277fee76578d5f4dada833916db4
peregin <info@peregin.com>
2014-11-04 22:58:11 +0100

    cosmetics

--- src/main/scala/peregin/gpv/gui/video/SeekableVideoPlayer.scala
+++ src/main/scala/peregin/gpv/gui/video/SeekableVideoPlayer.scala
@@ -20,18 +20,18 @@ class SeekableVideoPlayer(url: String, val telemetry: Telemetry,
   val video = new SeekableVideoStream(url)
 
   override def play() {
-    playerActor ! Play
+    playerActor ! PlayCommand
   }
 
 
   override def step() {
-    playerActor ! Play
+    playerActor ! PlayCommand
   }
 
   override def pause() = sys.error("not supported")
 
   override def seek(percentage: Double) {
-    playerActor ! Seek(percentage)
+    playerActor ! SeekCommand(percentage)
   }
 
   override def close() {
@@ -51,27 +51,27 @@ class SeekableVideoPlayer(url: String, val telemetry: Telemetry,
 
   val system = ActorSystem("gpv")
   val playerActor = system.actorOf(Props(new PlayerControllerActor(video, handleFrame)), name = "playerController")
-  playerActor ! Play
+  playerActor ! PlayCommand
 }
 
-sealed trait PlayerCommand
-case object Play extends PlayerCommand
-case class Seek(percentage: Double) extends PlayerCommand
+sealed trait ControllerCommand
+case object PlayCommand extends ControllerCommand
+case class SeekCommand(percentage: Double) extends ControllerCommand
 
 class PlayerControllerActor(video: SeekableVideoStream, listener: (FrameIsReady) => Unit) extends Actor with Logging {
 
   override def receive = {
-    case Play => video.readPacket.head match {
+    case PlayCommand => video.readPacket.head match {
       case frame @ FrameIsReady(tsInMillis, percentage, keyFrame, image) =>
         info(f"frame received, ts=${TimePrinter.printDuration(tsInMillis)}, @=$percentage%2.2f, keyFrame=$keyFrame")
         if (tsInMillis > 0) video.waitIfNeeded(tsInMillis)
         listener(frame)
         //self ! Play
       case EndOfStream => context.stop(self)
-      case ReadInProgress => self ! Play
+      case ReadInProgress => self ! PlayCommand
       case _ => // ignore
     }
-    case Seek(percentage) =>
+    case SeekCommand(percentage) =>
       video.seek(percentage).foreach{ seekFrame =>
         info(f"seek frame received, ts=${TimePrinter.printDuration(seekFrame.tsInMillis)}, @=${seekFrame.percentage%2.2f}")
         listener(seekFrame)
