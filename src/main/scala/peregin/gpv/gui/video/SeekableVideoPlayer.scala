8974a4b22c80d2955ab1673938f81d95018d86dc
peregin <info@peregin.com>
2014-11-06 21:40:43 +0100

    cancel play events

--- src/main/scala/peregin/gpv/gui/video/SeekableVideoPlayer.scala
+++ src/main/scala/peregin/gpv/gui/video/SeekableVideoPlayer.scala
@@ -1,7 +1,7 @@
 package peregin.gpv.gui.video
 
 import akka.actor._
-import peregin.gpv.gui.video.PlayerControllerActor.{Run, Idle, Data, State}
+import peregin.gpv.gui.video.PlayerControllerActor.{Run, Idle, State}
 
 import peregin.gpv.model.Telemetry
 import java.awt.Image
@@ -39,7 +39,7 @@ class SeekableVideoPlayer(url: String, val telemetry: Telemetry,
 
   val system = ActorSystem("gpv")
   val playerActor = system.actorOf(Props(new PlayerControllerActor(video, handleFrame)), name = "playerController")
-  playerActor ! PlayCommand
+  //playerActor ! PlayCommand
 }
 
 object PlayerProtocol {
@@ -54,34 +54,30 @@ object PlayerControllerActor {
   sealed trait State
   case object Idle extends State
   case object Run extends State
-
-  case class Data(packet: PacketReply)
 }
 
-class PlayerControllerActor(video: SeekableVideoStream, listener: (FrameIsReady) => Unit) extends Actor with FSM[State, Data] {
-
-  startWith(Idle, Data(ReadInProgress))
+class PlayerControllerActor(video: SeekableVideoStream, listener: (FrameIsReady) => Unit) extends Actor with FSM[State, PacketReply] {
 
   when(Idle) {
     case Event(StepCommand, _) => video.readNextFrame match {
       case Some(frame) =>
         log.debug(s"step to ts=${TimePrinter.printDuration(frame.tsInMillis)}")
         listener(frame)
-        stay using Data(frame)
-      case _ => stay using Data(EndOfStream)
+        stay using frame
+      case _ => stay using EndOfStream
     }
     case Event(SeekCommand(percentage), _) => video.seek(percentage) match {
       case Some(seekFrame) =>
         log.info(f"nearest frame found, ts=${TimePrinter.printDuration(seekFrame.tsInMillis)}, @=${seekFrame.percentage%2.2f}")
         listener(seekFrame)
-        stay using Data(seekFrame)
-      case _ => stay using Data(EndOfStream)
+        stay using seekFrame
+      case _ => stay using EndOfStream
     }
     case Event(PlayCommand, data) => goto(Run) using data
   }
 
   when(Run) {
-    case Event(_, data @ Data(EndOfStream)) =>
+    case Event(_, data @ EndOfStream) =>
       log.info("end of the stream has been reached")
       goto(Idle) using data
     case Event(PlayCommand, _) => video.readNextFrame match {
@@ -91,19 +87,22 @@ class PlayerControllerActor(video: SeekableVideoStream, listener: (FrameIsReady)
         listener(frame)
         val delay = video.markDelay(tsInMillis)
         import scala.concurrent.duration._
-        setTimer("next read", PlayCommand, delay millis, false)
-        stay using Data(frame)
-      case _ => goto(Idle) using Data(EndOfStream)
+        setTimer("nextread", PlayCommand, delay millis, repeat = false)
+        stay using frame
+      case _ => goto(Idle) using EndOfStream
     }
-    case Event(sc: SeekCommand, data) => goto(Idle) using data
+    case Event(sc: SeekCommand, data) =>
+      cancelTimer("nextread")
+      goto(Idle) using data
     case Event(PauseCommand, data) => goto(Idle) using data
   }
 
   whenUnhandled {
     case any =>
       log.warning(s"unhandled ${any.toString}")
-      stay
+      stay()
   }
 
+  startWith(Idle, ReadInProgress)
   initialize()
 }
