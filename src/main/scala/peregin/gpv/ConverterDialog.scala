7a9838b104e965f6644b3649f142426e763ea32c
peregin <info@peregin.com>
2014-11-14 13:54:00 +0100

    configure preferred size and handle escape key

--- src/main/scala/peregin/gpv/ConverterDialog.scala
+++ src/main/scala/peregin/gpv/ConverterDialog.scala
@@ -1,8 +1,9 @@
 package peregin.gpv
 
+import java.awt.Dimension
 import java.io.File
 
-import peregin.gpv.gui.{FileChooserPanel, MigPanel}
+import peregin.gpv.gui.{Goodies, FileChooserPanel, MigPanel}
 import peregin.gpv.util.Logging
 
 import scala.swing.{Dialog, Window}
@@ -12,12 +13,20 @@ class ConverterDialog(parent: Window = null) extends Dialog(parent) with Logging
 
   title = "Converter"
   modal = true
+  preferredSize = new Dimension(600, 300)
+
+  Goodies.mapEscapeTo(this, handleCancel)
 
   contents = new MigPanel("ins 5, fill", "[fill]", "[fill]") {
     add(new FileChooserPanel("Output", save, null), "")
   }
 
-  def save(file: File): Unit = {
+  def save(file: File) {
     log.info(s"save to ${file.getAbsolutePath}")
+  }
+
+  private def handleCancel() {
+    log.info("cancel")
+    close()
   }
 }
