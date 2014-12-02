5a013155757c83236505fb121ee4a2354115ca5a
peregin <info@peregin.com>
2014-12-02 19:45:32 +0100

    add property based testing for duration converter and cleanup duplicate tests

--- src/test/scala/peregin/gpv/util/TimePrinterSpec.scala
+++ src/test/scala/peregin/gpv/util/TimePrinterSpec.scala
@@ -1,12 +1,15 @@
 package peregin.gpv.util
 
+import org.joda.time.DateTime
+import org.scalacheck.Prop
+import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
-import TimePrinter._
+import peregin.gpv.util.TimePrinter._
 
 /**
  * Created by peregin on 04/11/14.
  */
-class TimePrinterSpec extends Specification {
+class TimePrinterSpec extends Specification with ScalaCheck {
 
   addArguments(stopOnFail)
 
@@ -18,5 +21,20 @@ class TimePrinterSpec extends Specification {
     "parse text to elapsed duration" in {
       text2Duration("00:01:02.001") === 62001
     }
+  }
+
+  "time printer" should {
+    "convert an arbitrary time" in {
+      val time = new DateTime(2014, 11, 20, 16, 1, 23, 100)
+      printTime(time.getMillis) === "04:01:23.100"
+    }
+  }
+
+  "converter" in {
+    "generate and parse the same timestamp" ! Prop.forAll { (ts: Int) =>
+      val text = printDuration(ts)
+      val reverseTs = text2Duration(text)
+      ts === reverseTs
+    }.set(minTestsOk = 100)
   }
 }
