ae972eec5bce05da3d5da4a339fbbb19f015699f
peregin <info@peregin.com>
2014-12-10 08:50:13 +0100

    draft a test for the player actor

--- src/test/scala/peregin/gpv/video/PlayerControllerActorSpec.scala
+++ src/test/scala/peregin/gpv/video/PlayerControllerActorSpec.scala
@@ -1,26 +1,35 @@
 package peregin.gpv.video
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.FSM.{CurrentState, SubscribeTransitionCallBack}
 import akka.actor.{Props, ActorSystem}
 import akka.testkit.{ImplicitSender, TestKit}
+import org.specs2.mock.Mockito
 import org.specs2.mutable.SpecificationLike
 import org.specs2.specification.AfterExample
+import peregin.gpv.util.Logging
+import peregin.gpv.video.PlayerControllerActor.Idle
+
+import scala.concurrent.duration.Duration
 
 
 class PlayerControllerActorSpec extends TestKit(ActorSystem("test-system"))
-  with SpecificationLike with AfterExample with ImplicitSender {
+  with SpecificationLike with AfterExample with ImplicitSender with Mockito with Logging {
+
+  val timeout = Duration(1, TimeUnit.SECONDS)
 
   "controller" should {
-    "start reading video stream" in {
 
-      val controller = system.actorOf(Props(new PlayerControllerActor(null, null)))
+    "start reading video stream" in {
+      val videoStream = mock[SeekableVideoStream]
+      val controller = system.actorOf(Props(new PlayerControllerActor(videoStream, null)))
       controller ! SubscribeTransitionCallBack(testActor)
 
-      expectMsgPF() {
-        case CurrentState(_, ReadInProgress) => true
-        case _ => false
-      }
-    }.pendingUntilFixed("in progress")
+      expectMsg(timeout, CurrentState(controller, Idle))
+
+      ok("succeeded")
+    }//.pendingUntilFixed("in progress")
   }
 
   override protected def after = system.shutdown()
